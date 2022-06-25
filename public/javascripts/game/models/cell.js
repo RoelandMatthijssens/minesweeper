5b7667fcf642411401ef06e0d8972d17968f7be5
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2022-06-25 15:52:53 +0200

    Add colours

--- public/javascripts/game/models/cell.js
+++ public/javascripts/game/models/cell.js
@@ -4,12 +4,62 @@ class Cell {
   }
 
   render(x, y, cell_size) {
-    console.log("rendering cell");
+    this.draw_border(x, y, cell_size);
+    this.draw_symbol(x, y, cell_size);
+  }
+
+  draw_border(x, y, cell_size) {
     push();
-    stroke(color("green"));
+    strokeWeight(4);
+    stroke(color("darkGray"));
+    fill(color("lightGray"));
     rect(x, y, cell_size, cell_size);
-    textAlign(CENTER);
-    text(this.value, x + cell_size / 2, y + cell_size / 2);
     pop();
+  }
+
+  draw_symbol(x, y, cell_size) {
+    const color = this.symbol_color();
+    push();
+    stroke(color);
+    fill(color);
+    strokeWeight(2);
+    textSize(Math.floor(cell_size / 2.5));
+    textAlign(CENTER, CENTER);
+    text(this.symbol(), x + cell_size / 2, y + cell_size / 2);
+    pop();
+  }
+
+  symbol() {
+    return {
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+      0: "",
+      flag: "🚩",
+      bomb: "💣",
+    }[this.value];
+  }
+
+  symbol_color() {
+    return {
+      1: "blue",
+      2: "green",
+      3: "crimson",
+      4: "darkBlue",
+      5: "darkRed",
+      6: "darkCyan",
+      7: "black",
+      8: "darkGrey",
+      9: "fuchsia",
+      0: "white",
+      flag: "lightGreen",
+      bomb: "black",
+    }[this.value];
   }
 }
