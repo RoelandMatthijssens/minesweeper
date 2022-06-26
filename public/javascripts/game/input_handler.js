ce7ccb83167bbc1bbd0fc277cbce0a4b7103166a
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2022-06-26 12:39:49 +0200

    Adds zoom to current mouse location

--- public/javascripts/game/input_handler.js
+++ public/javascripts/game/input_handler.js
@@ -3,11 +3,11 @@ class InputHandler {
     this.viewport = viewport;
     this.canvas = canvas;
     this.setup();
+    this.min_zoom = 0.2;
+    this.max_zoom = 3;
   }
 
   setup() {
-    console.log(this);
-    console.log("aaa");
     window.mousePressed = (event) => {
       console.log("mouse pressed");
     };
@@ -22,14 +22,22 @@ class InputHandler {
 
   handle_zoom(scroll_event) {
     const { x, y, deltaY } = scroll_event;
+    const { x: viewport_x, y: viewport_y, zoom: viewport_zoom } = this.viewport;
+    const { height, width } = this.canvas;
 
-    const direction = deltaY > 0 ? "up" : "down";
-    const current_zoom = this.viewport.zoom;
+    const direction = deltaY > 0 ? -1 : 1;
+    const zoom_factor = 0.05;
+    const zoom_delta = 1 * direction * zoom_factor;
 
-    if (direction == "up") {
-      this.viewport.zoom = Math.min(current_zoom + 0.2, 3);
-    } else {
-      this.viewport.zoom = Math.max(0.2, current_zoom - 0.2);
-    }
+    const x_delta = (x - viewport_x) / (width * viewport_zoom);
+    const y_delta = (y - viewport_y) / (height * viewport_zoom);
+
+    this.viewport.x -= x_delta * width * zoom_delta;
+    this.viewport.y -= y_delta * height * zoom_delta;
+    this.viewport.zoom = constrain(
+      viewport_zoom + zoom_delta,
+      this.min_zoom,
+      this.max_zoom
+    );
   }
 }
