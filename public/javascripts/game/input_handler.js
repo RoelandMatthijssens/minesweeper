9828b47c780c9441a2c47d0596828716fed4c401
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2022-06-26 16:25:30 +0200

    Handle clicks on individual cells

--- public/javascripts/game/input_handler.js
+++ public/javascripts/game/input_handler.js
@@ -6,25 +6,19 @@ class InputHandler {
     this.min_zoom = 0.2;
     this.max_zoom = 3;
     this.is_dragging = false;
+    this.did_drag = false;
     this.previous_x = 0;
     this.previous_y = 0;
 
     this.zoom_handler = null;
     this.pan_handler = null;
+    this.click_handler = null;
   }
 
   setup() {
-    window.mousePressed = (event) => {
-      this.is_dragging = true;
-      this.previous_x = event.clientX;
-      this.previous_y = event.clientY;
-    };
+    window.mousePressed = this.prepare_drag.bind(this);
     window.mouseDragged = this.handle_drag.bind(this);
-    window.mouseReleased = (event) => {
-      this.previous_x = null;
-      this.previous_y = null;
-      this.is_dragging = false;
-    };
+    window.mouseReleased = this.handle_click.bind(this);
     this.canvas.mouseWheel(this.handle_scroll.bind(this));
   }
 
@@ -36,6 +30,7 @@ class InputHandler {
 
   handle_drag(drag_event) {
     this.is_dragging = true;
+    this.did_drag = true;
     const new_x = drag_event.clientX;
     const new_y = drag_event.clientY;
     const dx = new_x - this.previous_x;
@@ -43,5 +38,24 @@ class InputHandler {
     this.previous_x = new_x;
     this.previous_y = new_y;
     this.pan_handler(dx, dy);
+  }
+
+  handle_click(click_event) {
+    this.previous_x = null;
+    this.previous_y = null;
+    this.is_dragging = false;
+    if (this.did_drag) {
+      this.did_drag = false;
+      return;
+    }
+    const { clientX: x, clientY: y } = click_event;
+    this.click_handler(x, y);
+  }
+  prepare_drag(click_event) {
+    // drag events only have the destination position of the drag, not the start position. In order to calculate the
+    // correct drag delta we need to keep track of where we started the drag, aka, the initial click position.
+    // This method stores it temporarily in case we do start dragging.
+    this.previous_x = click_event.clientX;
+    this.previous_y = click_event.clientY;
   }
 }
--- public/javascripts/game/models/world.js
+++ public/javascripts/game/models/world.js
@@ -10,20 +10,57 @@ class World {
       this.viewport.zoom(x, y, d, this.canvas.width, this.canvas.height);
     };
     this.input_handler.pan_handler = this.viewport.pan.bind(this.viewport);
+    this.input_handler.click_handler = this.handle_click.bind(this);
   }
 
-  initialize(world_data) {
-    const { chunk_size } = world_data;
+  initialize(data) {
+    const { chunk_size } = data;
     this.chunk_size = chunk_size;
-    world_data["chunks"].forEach((chunk_data) => {
-      this.chunks.push(new Chunk(chunk_data, chunk_size));
+    data["chunks"].forEach((chunk_data) => {
+      this.chunks.push(new Chunk(chunk_data, chunk_size, this.cell_size));
     });
   }
 
   render() {
     this.viewport.apply();
     this.chunks.forEach((chunk) => {
-      chunk.render(this.cell_size);
+      chunk.render();
     });
+  }
+
+  handle_click(x, y) {
+    const chunk = this.get_chunk(x, y);
+    const cell_index = this.get_index_in_chunk(chunk, x, y);
+    if (chunk != null && cell_index != null) {
+      chunk.clicked(cell_index);
+    }
+  }
+
+  get_chunk(abs_x, abs_y) {
+    let result = null;
+    const { x, y } = this.viewport.absolute_to_relative(abs_x, abs_y);
+    this.chunks.some((chunk) => {
+      if (
+        x > chunk.x * chunk.dimensions() &&
+        x < (chunk.x + 1) * chunk.dimensions() &&
+        y > chunk.y * chunk.dimensions() &&
+        y < (chunk.y + 1) * chunk.dimensions()
+      ) {
+        result = chunk;
+      }
+    });
+    return result;
+  }
+
+  get_index_in_chunk(chunk, abs_x, abs_y) {
+    if (!chunk) {
+      return;
+    }
+    const { x, y } = this.viewport.absolute_to_relative(abs_x, abs_y);
+    const dx_cell = x - chunk.x * chunk.dimensions();
+    const dy_cell = y - chunk.y * chunk.dimensions();
+    const cell_x_index = Math.floor(dx_cell / this.cell_size);
+    const cell_y_index = Math.floor(dy_cell / this.cell_size);
+    return cell_y_index * this.chunk_size + cell_x_index;
   }
 }
